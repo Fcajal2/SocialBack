fc0c6e6866546d9e7abc02b130fee621fbefa027
Fcajal2 <daikuzu@hotmail.com>
2023-02-09 12:01:07 -0300

    changed

--- routes/postRouter.ts
+++ routes/postRouter.ts
@@ -4,18 +4,18 @@ import deletePost from "../controllers/postController/deletePost";
 import editPost from "../controllers/postController/editPost";
 import getAllPosts from "../controllers/postController/getAllPosts";
 import getPost from "../controllers/postController/getPost";
-import getUserPosts from "../controllers/postController/getUserPosts";
 import likePost from "../controllers/postController/likePost";
+import repostPost from "../controllers/postController/repostPost";
 import checkLogin from "../middlewares/checkLogin";
 
 const postRouter = Router();
 
 postRouter.post("/create", checkLogin, createPost);
-postRouter.put("/:id", checkLogin, editPost);
 postRouter.delete("/:id", checkLogin, deletePost);
+postRouter.put("/:id", checkLogin, editPost);
 postRouter.get("/", getAllPosts);
 postRouter.get("/:id", getPost);
-postRouter.post("/:user_id", getUserPosts);
 postRouter.post("/:id/like", checkLogin, likePost);
+postRouter.post("/:id/repost", checkLogin, repostPost);
 
 export default postRouter;
--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,15 +1,26 @@
 import { Router } from "express";
 import createUser from "../controllers/userController/createUser";
+import deleteUser from "../controllers/userController/deleteUser";
+import editUser from "../controllers/userController/editUser";
 import followUser from "../controllers/userController/followUser";
-import getUser from "../controllers/userController/getUser";
 import loginUser from "../controllers/userController/loginUser";
+import uploadFile from "../middlewares/uploadFile";
 import checkLogin from "../middlewares/checkLogin";
+import getUserInfo from "../controllers/userController/getUserInfo";
+import getFollowers from "../controllers/userController/getFollowers";
+import getFollowings from "../controllers/userController/getFollowings";
+import getUserPosts from "../controllers/userController/getUserPosts";
 
 const userRouter = Router();
 
 userRouter.post("/register", createUser);
-userRouter.post("/login", loginUser);
-userRouter.get("/:id", getUser);
+userRouter.delete("/", checkLogin, deleteUser);
+userRouter.put("/", checkLogin, uploadFile.single("image_file"), editUser);
 userRouter.post("/:id/follow", checkLogin, followUser);
+userRouter.get("/:id/followers", getFollowers);
+userRouter.get("/:id/followings", getFollowings);
+userRouter.get("/:id", getUserInfo);
+userRouter.get("/:id/posts", getUserPosts)
+userRouter.post("/login", loginUser);
 
 export default userRouter;
